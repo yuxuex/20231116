4f9e3b8383b01651eb03006fabf850816602e9c1
yuxuex <s1092457@gm.pu.edu.tw>
2023-11-23 10:15:38 +0800

    www

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>向育學網頁</h1>"
+    homepage = "<h1>向育學網頁2023/11/23</h1>"
     homepage += "<a href=/mis>科目</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome>傳送使用者暱稱</a><br>"
